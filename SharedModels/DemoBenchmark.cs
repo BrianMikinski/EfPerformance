2959b5a628d3c81122ba5c185be72bcf6555d757
Brian Mikinski <brian.mikinski@gmail.com>
2022-03-29 22:03:07 -0500

    Configure the demo benchmark

--- SharedModels/DemoBenchmark.cs
+++ SharedModels/DemoBenchmark.cs
@@ -1,5 +1,6 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
+using System.Text;
 
 namespace Blog.Benchmarks;
 
@@ -10,36 +11,46 @@ public class DemoBenchmark : BenchmarkBase
     [GlobalSetup]
     public void GlobalSetup()
     {
-       
+       // code that is run once prior to all BenchmarkDotNet iterations
     }
 
     [IterationSetup]
     public void IterationSetup()
     {
-        
+        // code that is run once prior to each iteration
     }
 
-    [BenchmarkCategory(nameof(DemoBenchmark)), Benchmark]
-    public void NewFunction()
-    {
-
-    }
-
-    [BenchmarkCategory(nameof(DemoBenchmark)), Benchmark(Baseline = true)]
+    [BenchmarkCategory(nameof(DemoBenchmark))]
+    [Benchmark(Baseline = true)]
     public void OldFunction()
     {
+        string firstName = "John";
+        string lastName = "Doe";
+        string name = firstName + " " + lastName;
+    }
 
+    [BenchmarkCategory(nameof(DemoBenchmark))]
+    [Benchmark]
+    public void NewFunction()
+    {
+        string firstName = "John";
+        string lastName = "Doe";
+        StringBuilder sb = new(firstName);
+
+        string name = sb.Append(" ")
+            .Append(lastName)
+            .ToString();
     }
 
     [IterationCleanup]
     public void IterationCleanup()
     {
-
+        // code run once after each iteration
     }
 
     [GlobalCleanup]
     public void GlobalCleanup()
     {
-        
+       // code run once after all iterations have run
     }
 }
